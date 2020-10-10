4338f6e41d9dd9e77e10c169ec28b78e33d19817
unlomtrois <vadim.kuz02@gmail.com>
2020-10-10 16:07:59 +0300

    add ALL option

--- src/cli.ts
+++ src/cli.ts
@@ -3,6 +3,10 @@
 import commander from 'commander';
 import IO from './io/io.js';
 import Suggest from './suggest.js';
+import { execSync } from 'child_process';
+import chalk from 'chalk';
+
+const ctx = new chalk.Instance({ level: 1 });
 
 /**
  * Модуль CLI
@@ -34,6 +38,7 @@ class CLI {
   private initCLIOptions = () => {
     this.cli
       .option<number>('-l, --limit <number>', 'ограничение по поиску', parseFloat, 2000)
+      .option('-A, --all', 'выполнить все остальные комманды автоматически', 'Россия')
       .option('-a, --area <area-name>', 'название территории поиска или индекс', 'Россия')
       .option('-L, --language <язык>', 'язык локализации', 'RU')
       .option('-C, --cluster', 'поиск по кластерам')
@@ -48,17 +53,28 @@ class CLI {
       .command('search <text>')
       .description('поиск вакансий по полю text')
       .action(async (text: string) => {
-        this.suggest.silent_mode = this.cli.silent;
-
         const area = await this.suggest.area(this.cli.area, this.cli.language);
 
-        this.io.search({
+        await this.io.search({
           text: text,
           area: area,
           limit: this.cli.limit,
-          cluster: this.cli.cluster ?? false
+          cluster: this.cli.cluster ?? this.cli.all ?? false
         });
-      })
+
+        if (this.cli.all) {
+          setTimeout(() => {
+            console.log(ctx.yellow('\n>'), 'node cli.js get-full');
+            execSync('node cli.js get-full', { stdio: 'inherit' });
+
+            console.log(ctx.yellow('\n>'), 'node cli.js prepare');
+            execSync('node cli.js prepare', { stdio: 'inherit' });
+
+            console.log(ctx.yellow('\n>'), 'node cli.js analyze');
+            execSync('node cli.js analyze', { stdio: 'inherit' });
+          }, 1000);
+        }
+      });
 
     this.cli
       .command('get-full')
